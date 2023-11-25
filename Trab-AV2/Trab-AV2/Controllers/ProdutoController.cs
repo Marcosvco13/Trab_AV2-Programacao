7d15ba71feb8d1430a41aa20551f2c66a786e087
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2023-11-25 16:26:20 -0300

    0.9
    Criação do index de produto

--- Trab-AV2/Trab-AV2/Controllers/ProdutoController.cs
+++ Trab-AV2/Trab-AV2/Controllers/ProdutoController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Trab_AV2.Model.Services;
+using Trab_AV2.VM;
 
 namespace Trab_AV2.Controllers
 {
     public class ProdutoController : Controller
     {
+        private ServiceProduto _ServiceProduto;
+        public ProdutoController()
+        {
+            _ServiceProduto = new ServiceProduto();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(ProdutoVM.ListarProdutos());
         }
     }
 }
--- Trab-AV2/Trab-AV2/VM/ProdutoVM.cs
+++ Trab-AV2/Trab-AV2/VM/ProdutoVM.cs
@@ -5,8 +5,27 @@ namespace Trab_AV2.VM
     public class ProdutoVM
     {
         public int IDProduto { get; set; }
-        public string NomeProduto { get; set; }
-        public DateTime? DataCadastro { get; set; }
+        public string? IDUsuario {  get; set; }
+        public string? NomeProduto { get; set; }
+        public string? Quantidade { get; set; }
+        public decimal? ValorProduto { get; set; }
+        public string? Disponivel { get; set; }
 
+        public static List<ProdutoVM> ListarProdutos()
+        {
+            var db = new PADARIA_AV2Context();
+            return (from Produto in db.Produtos
+                    join SimNao in db.SimNaos on Produto.Disp
+                    equals SimNao.Id
+                    select new ProdutoVM
+                    {
+                        IDProduto = Produto.Id,
+                        IDUsuario = Produto.IdUser,
+                        NomeProduto = Produto.NmProduto,
+                        Quantidade = Produto.Qtd,
+                        ValorProduto = Produto.Valor,
+                        Disponivel = SimNao.Descricao,
+                    }).ToList();
+        }
     }
 }
