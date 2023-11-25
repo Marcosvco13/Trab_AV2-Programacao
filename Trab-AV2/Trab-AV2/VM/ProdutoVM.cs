589a99e0422df8848ebe74eb05e570c4b0299f4e
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2023-11-25 16:03:57 -0300

    0.8
    Ajustes da VM Produto

--- Trab-AV2/Trab-AV2/VM/ProdutoVM.cs
+++ Trab-AV2/Trab-AV2/VM/ProdutoVM.cs
@@ -4,5 +4,9 @@ namespace Trab_AV2.VM
 {
     public class ProdutoVM
     {
+        public int IDProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public DateTime? DataCadastro { get; set; }
+
     }
 }
