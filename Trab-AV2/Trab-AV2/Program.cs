5a44c73fc2cd109fe0136366da8287575b131a7f
Daniel Campos <contatodanielperalba@hotmail.com>
2023-11-25 16:01:33 -0300

    Implementando Registro e Login da aplicacao

--- Trab-AV2/Trab-AV2/Program.cs
+++ Trab-AV2/Trab-AV2/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Trab_AV2.Areas.Identity.Data;
+using Trab_AV2.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Trab_AV2ContextConnection") ?? throw new InvalidOperationException("Connection string 'Trab_AV2ContextConnection' not found.");
+
+builder.Services.AddDbContext<Trab_AV2Context>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<Trab_AV2User>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<Trab_AV2Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,11 +26,12 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
