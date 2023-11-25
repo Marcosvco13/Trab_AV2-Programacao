67be84457d9b3630e77817e06f3b0ca46777101f
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2023-11-25 13:50:35 -0300

    0.2
    Atualização dos Nuget do projeto, versão utilizada 7.0.14 no .Net 7

--- Trab-AV2/Trab-AV2.Model/obj/Debug/net7.0/Trab-AV2.Model.AssemblyInfo.cs
+++ Trab-AV2/Trab-AV2.Model/obj/Debug/net7.0/Trab-AV2.Model.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trab-AV2.Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44e0375edd5449b8a1aa6822cbbc2497c09314b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trab-AV2.Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trab-AV2.Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
