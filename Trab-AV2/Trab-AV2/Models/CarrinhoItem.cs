4d00db0a6bf157f210276e089a09de235bdd4a21
Daniel Campos <contatodanielperalba@hotmail.com>
2023-11-26 17:40:03 -0300

    Implementando função de cadastro no carrinho

---
+++ Trab-AV2/Trab-AV2/Models/CarrinhoItem.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trab_AV2.Models
+{
+    public class CarrinhoItem
+    {
+    }
+}
---
+++ Trab-AV2/Trab-AV2/Models/ItemVenda.cs
@@ -0,0 +1,10 @@
+﻿using Trab_AV2.Model.Models;
+
+namespace Trab_AV2.Controllers
+{
+    internal class ItemVenda : Venda
+    {
+        public int VendaId { get; set; }
+        public int ProdutoId { get; set; }
+    }
+}
