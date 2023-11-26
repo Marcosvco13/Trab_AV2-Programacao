15b676ec1a099f6eafdd7e55661d2ae89ab4206f
Daniel Campos <contatodanielperalba@hotmail.com>
2023-11-26 18:01:52 -0300

    Pequenos ajustes

--- Trab-AV2/Trab-AV2/Controllers/VendaController.cs
+++ Trab-AV2/Trab-AV2/Controllers/VendaController.cs
@@ -38,53 +38,12 @@ namespace Trab_AV2.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Venda venda, int[] produtosSelecionados)
+        public async Task<IActionResult> Create(Venda venda)
         {
             if (ModelState.IsValid)
             {
-                // Lógica para criar uma nova venda
-                var novaVenda = new Venda
-                {
-                    // Defina os outros campos da venda conforme necessário
-                    IdUser = venda.IdUser,
-                    DataVenda = DateTime.Now,
-                    // ...
-
-                    // Adicione a lógica para calcular o valor total da venda, se necessário
-                    ValorVenda = 0 // Por enquanto, vamos inicializar como 0
-                };
-
-                // Adicione a nova venda ao contexto
-                await _ServiceVenda.oRepositoryVenda.IncluirAsync(novaVenda);
-
-                // Agora, adicione os produtos selecionados à venda
-                foreach (var idProduto in produtosSelecionados)
-                {
-                    var produto = _ServiceVenda.oRepositoryProduto.SelecionarPk(idProduto);
-
-                    // Verifica se o produto existe e se está disponível
-                    if (produto != null && produto.Disp > 0)
-                    {
-                        // Cria um novo item de venda associado ao produto
-                        var itemVenda = new ItemVenda
-                        {
-                            VendaId = novaVenda.Id, // ID da nova venda criada
-                            ProdutoId = produto.Id,
-                            // Adicione outras propriedades do item de venda, se necessário
-                        };
-
-                        // Adicione o item de venda ao contexto
-                        await _ServiceVenda.oRepositoryVenda.IncluirAsync(itemVenda);
-
-                        // Atualize o valor total da venda ao adicionar o valor do produto
-                        novaVenda.ValorVenda += produto.Valor;
-                    }
-                }
-
-                // Atualize a venda com o valor total
-                await _ServiceVenda.oRepositoryVenda.AlterarAsync(novaVenda);
-
-                return RedirectToAction("Detail", new { id = novaVenda.Id });
+                var vendaEfetuada = await _ServiceVenda.oRepositoryVenda.IncluirAsync(venda);
+                return View(vendaEfetuada);
             }
             else
             {
